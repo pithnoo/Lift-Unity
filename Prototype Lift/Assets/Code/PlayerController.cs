4fefe88aab40528a1384083c409a3afa51dc9073
pithnoo <62898754+pithnoo@users.noreply.github.com>
2020-09-25 15:58:50 +0100

    Add empty project

---
+++ Prototype Lift/Assets/Code/PlayerController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+    public Rigidbody2D myRigidBody;
+    public Vector2 movement;
+
+    void Start(){
+        myRigidBody = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+    }
+
+    void FixedUpdate(){
+        myRigidBody.MovePosition(myRigidBody.position + movement * moveSpeed * Time.fixedDeltaTime);
+    }
+}
