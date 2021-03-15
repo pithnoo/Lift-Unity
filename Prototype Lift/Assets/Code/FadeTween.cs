f4e51982915e13ea26020f875b448f530f6ed495
pithnoo <62898754+pithnoo@users.noreply.github.com>
2021-03-15 21:24:12 +0000

    adjustment to tweening

--- Prototype Lift/Assets/Code/FadeTween.cs
+++ Prototype Lift/Assets/Code/FadeTween.cs
@@ -1,15 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class FadeTween : MonoBehaviour
 {
     public float delay;
     public float duration;
-    public float opacity;
+    public float inOpacity;
+    public float outOpacity;
     public LeanTweenType tweenType;
+    public Image imageFade;
     
     void Start(){
-        LeanTween.alpha(gameObject, 1f, duration).setDelay(delay).setEase(tweenType);
+        LeanTween.alpha(imageFade.rectTransform, inOpacity, 1f).setDelay(delay).setEase(tweenType).setOnComplete(fadeOut);
+        //LeanTween.alpha(imageFade2.rectTransform, inOpacity, 1f).setDelay(delay).setEase(tweenType).setOnComplete(fadeOut);
+    }
+
+    void fadeOut(){
+        LeanTween.alpha(imageFade.rectTransform, outOpacity, 1f).setDelay(delay).setEase(tweenType);
+        //LeanTween.alpha(imageFade2.rectTransform, outOpacity, 1f).setDelay(delay).setEase(tweenType).setOnComplete(fadeOut);
     }
 }
