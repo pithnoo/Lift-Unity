ffe8d10765adabe2a2c4b980bc99500aad8d6e23
pithnoo <62898754+pithnoo@users.noreply.github.com>
2021-03-19 14:55:38 +0000

    updated weapon select screen
    player can now select weapons from the main menu

--- Prototype Lift/Assets/Code/Player/WeaponSwitching.cs
+++ Prototype Lift/Assets/Code/Player/WeaponSwitching.cs
@@ -9,6 +9,7 @@ public class WeaponSwitching : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        selectedWeapon = PlayerPrefs.GetInt("SelectedWeapon");
         SelectWeapon(selectedWeapon);
     }
 
--- Prototype Lift/Assets/Code/UI/Menu.cs
+++ Prototype Lift/Assets/Code/UI/Menu.cs
@@ -1,7 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.UI;
 public class Menu : MonoBehaviour
 {
     public LevelLoader levelLoader;
@@ -9,23 +9,25 @@ public class Menu : MonoBehaviour
     public GameObject instructions;
     public GameObject instructions2;
     public GameObject weaponSelectScreen;
+    
     // Start is called before the first frame update
     void Start()
     {
         levelLoader = FindObjectOfType<LevelLoader>();
         audioManager = FindObjectOfType<AudioManager>();
+        PlayerPrefs.DeleteAll();
     }
 
     public void StartGame(){
         //start game
         weaponSelectScreen.SetActive(true);
-        audioManager.Play("Select");
+        FindObjectOfType<AudioManager>().Play("Select");
     }
 
     public void LoadInstructions(){
         //load instructions
         instructions.SetActive(true);
-        audioManager.Play("Select");
+        FindObjectOfType<AudioManager>().Play("Select");
     }
     public void QuitGame(){
         //audioManager.Play("Pause");
@@ -35,16 +37,22 @@ public class Menu : MonoBehaviour
 
     public void ExitInstructions(){
         instructions.SetActive(false);
-        audioManager.Play("Select");
+        FindObjectOfType<AudioManager>().Play("Select");
     }
 
     public void ExitInstructions2(){
         instructions2.SetActive(false);
-        audioManager.Play("Select");
+        FindObjectOfType<AudioManager>().Play("Select");
     }
 
     public void ExitWeaponSelect(){
         weaponSelectScreen.SetActive(false);
-        audioManager.Play("Select");
+        FindObjectOfType<AudioManager>().Play("Select");
+    }
+
+    public void SelectWeapon(int selectedWeapon){
+        PlayerPrefs.SetInt("SelectedWeapon", selectedWeapon);
+        FindObjectOfType<AudioManager>().Play("Select");
+        levelLoader.loadLevel(1);
     }
 }
